2930da50470bd9d8251021d355e5fd68adc012c2
Bidhan Hansda <bidhanhansda1997@gmail.com>
2025-02-07 18:56:04 +0530

    chore: initialize project with folder structure setup

---
+++ constants/icons.ts
@@ -0,0 +1,38 @@
+import home from "@/assets/icons/home.png";
+import search from "@/assets/icons/search.png";
+import person from "@/assets/icons/person.png";
+import bell from "@/assets/icons/bell.png";
+import filter from "@/assets/icons/filter.png";
+import star from "@/assets/icons/star.png";
+import heart from "@/assets/icons/heart.png";
+import backArrow from "@/assets/icons/back-arrow.png";
+import calendar from "@/assets/icons/calendar.png";
+import info from "@/assets/icons/info.png";
+import logout from "@/assets/icons/logout.png";
+import people from "@/assets/icons/people.png";
+import shield from "@/assets/icons/shield.png";
+import wallet from "@/assets/icons/wallet.png";
+import rightArrow from "@/assets/icons/right-arrow.png";
+import area from "@/assets/icons/area.png";
+import edit from "@/assets/icons/edit.png";
+
+export default {
+  home,
+  search,
+  person,
+  bell,
+  filter,
+  star,
+  heart,
+  backArrow,
+  calendar,
+  info,
+  logout,
+  people,
+  shield,
+  wallet,
+  rightArrow,
+  area,
+  location,
+  edit,
+};
